765216576b65dfb5895d32a37123b704ae524345
kannadan <joonas.sou@gmail.com>
2017-10-18 15:56:24 +0300

    Google calendar post done
    *Google api credentials done
    *Credential check and post function done
    *needs some bug fixing but otherwise the script is done

--- seleniumSync.py
+++ seleniumSync.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from pyvirtualdisplay import Display
 from selenium import webdriver
 import getpass
@@ -5,6 +6,57 @@ from selenium.webdriver.common.keys import Keys
 import time
 from bs4 import BeautifulSoup
 from datetime import date, timedelta, datetime
+from datetime import time as time2
+
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import httplib2
+import os
+
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
 
 
 def login(addr, username, password):
@@ -39,7 +91,9 @@ def makeCalender(driver):
     startD, startM = map(lambda x : int(x), start.strip().strip(".").split("."))
     dateS = date(now.year, startM, startD)
 
-    for i in range(2): #sets how many weeks worth of data we collect
+    allEvents = []
+
+    for i in range(1): #sets how many weeks worth of data we collect
         for pointer, day in enumerate(days):
             paiva = (dateS + timedelta(pointer)).isoformat()
             events = day.findAll("div", {"class":"rbc-event"})
@@ -48,8 +102,24 @@ def makeCalender(driver):
                 labelL = event.find("div", {"class":"rbc-event-label-left"}).getText() #time
                 labelR = event.find("div", {"class":"rbc-event-label-right"}).getText() #class room
                 content = event.find("div", {"class":"rbc-event-content"}).getText()    #course
-                print("kello: " + labelL + " - Luokka: " + labelR)
-                print(content + "\n")
+
+                start, end = labelL.split("-")
+                hourS, minS = map(lambda x : int(x), start.strip().split(":"))
+                hourE, minE = map(lambda x : int(x), end.strip().split(":"))
+
+                start = datetime.combine(dateS + timedelta(pointer), time2(hourS, minS))
+                end = datetime.combine(dateS + timedelta(pointer), time2(hourE, minE))
+
+                dict = {}
+                dict["summary"] = content
+                dict["location"] = labelR
+                dict["start"] = {"dateTime" : start.strftime("%Y-%m-%dT%H:%M:%S"),
+                                    'timeZone': 'Europe/Helsinki'}
+                dict["end"] = {"dateTime" : end.strftime("%Y-%m-%dT%H:%M:%S"),
+                                    'timeZone': 'Europe/Helsinki'}
+                allEvents.append(dict)
+                """print("kello: " + labelL + " - Luokka: " + labelR)
+                print(content + "\n")"""
 
 
         #print("Days found " + str(len(days)))
@@ -63,10 +133,17 @@ def makeCalender(driver):
 
         #need to return a list or a class or something that has info on all of the events.
         #will get back on this once google calenders required format is known
+    return allEvents
 
 def googlify(stuff):
     #send events to google calenders
-    pass
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    for event in stuff:
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print ('Event created: %s' % (event.get('htmlLink')))
 
 if __name__ == "__main__":
     username = input("giv username: ")
@@ -76,7 +153,10 @@ if __name__ == "__main__":
     if browser.title != "Aapo":
         print("Wrong username or password")
     else:
-        makeCalender(browser)
+        events = makeCalender(browser)
+        googlify(events)
+
+
         input("just waiting around ")
     browser.quit()
     display.stop()
