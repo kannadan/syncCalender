9076cc0d64d114eadc297c6a3af066952853ca1a
kannadan <joonas.sou@gmail.com>
2017-10-19 10:32:11 +0300

    Comments

--- seleniumSync.py
+++ seleniumSync.py
@@ -120,7 +120,7 @@ def makeCalender(driver, week):
                                     'timeZone': 'Europe/Helsinki'}
                 dict["end"] = {"dateTime" : end.strftime("%Y-%m-%dT%H:%M:%S"),
                                     'timeZone': 'Europe/Helsinki'}
-                dict["reminders"] = {"useDefault": False,
+                dict["reminders"] = {"useDefault": False,       #set to True and get notification 30 min in advance
                                     "overrides" : []}
                 allEvents.append(dict)
                 print("event found on " + start.strftime("%d.%m.%Y"))
